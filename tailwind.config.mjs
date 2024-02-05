5a943574b1d97684acbdbfca0a44cc5ac90d8b86
notcharliee <saunderscharlie@hotmail.com>
2024-02-05 08:25:45 +0000

    styles: add base.css & scroll anim

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,11 +3,20 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			animation: {
+				"scroll": "scroll 20s linear infinite"
+			},
 			colors: {
 				"background": "#FEF6E6",
-				"foreground": "#141413",
-				"accent": "#4E4E4B",
-			}
+				"primary": "#141413",
+				"secondary": "#4E4E4B",
+			},
+			keyframes: {
+				"scroll": {
+					"0%": { "transform": "translateY(0%);" },
+					"100%": { "transform": "translateY(calc(216px * -6));" }
+				}
+			},
 		},
 	},
 	plugins: [],
