1f09256088236cdb3bbbb92eecc309150bc88ec8
notcharliee <saunderscharlie@hotmail.com>
2024-05-13 07:39:03 +0100

    feat: add mobile carousel

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,27 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			animation: {
-				"scroll": "scroll 20s linear infinite"
-			},
-			colors: {
-				"background": "#FEF6E6",
-				"primary": "#141413",
-				"secondary": "#4E4E4B",
-			},
-			fontFamily: {
-				"serif": ["DM Serif Text", "serif"],
-				"display": ["DM Serif Display", "serif"],
-			},
-			keyframes: {
-				"scroll": {
-					"0%": { "transform": "translateY(0%);" },
-					"100%": { "transform": "translateY(calc(216px * -6));" }
-				}
-			},
-		},
-	},
-	plugins: [],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      animation: {
+        scroll: "scroll 20s linear infinite",
+      },
+      colors: {
+        background: "#FEF6E6",
+        primary: "#141413",
+        secondary: "#4E4E4B",
+      },
+      fontFamily: {
+        serif: ["DM Serif Text", "serif"],
+        display: ["DM Serif Display", "serif"],
+      },
+      keyframes: {
+				horizontalScroll: {
+          to: { transform: "translateX(calc(-50% - 0.5rem));" },
+        },
+        verticalScroll: {
+          to: { transform: "translateY(calc(-50% - 0.5rem));" },
+        },
+      },
+    },
+  },
+  plugins: [],
 }
